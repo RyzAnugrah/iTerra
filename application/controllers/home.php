25773792273c5659f038c5beb5254d9e8d5443db
RyzAnugrah <asep.anugerah50@gmail.com>
2020-11-09 00:28:41 +0700

    add home aja

---
+++ application/controllers/home.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class home extends CI_Controller {
+	
+	// Index login
+	public function index() {
+		$this->load->view('home_view');
+	}
+	
+	// Fungsi lain
+	
+}
---
+++ application/views/home_view.php
@@ -0,0 +1,440 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<title>iTerra</title>
+	<link rel="icon" type="image/logo_title" href="assets\images\logo\iterra_bulet-01.png">
+
+	<!-- Font Awesome icons (free version)-->
+	<script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+
+	<!-- Fonts-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+	<link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+
+	<!-- Third party plugin CSS-->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+
+	<!--CSS (includes Bootstrap)-->
+	<link rel="stylesheet" href="css/styles.css" />
+	<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+	<style>
+		html {
+			scroll-behavior: smooth;
+		}
+	</style>
+</head>
+
+<body id="page-top">
+	<!-- Navigation-->
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+		<div class="containers">
+			<a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets\images\logo\iterra-01.png" style="width: 100px;" </a> <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto my-2 my-lg-0">
+						<li class="nav-item"><a class="nav-link js-scroll-trigger"></a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="#artikel">ARTIKEL</a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="#kampanye">KAMPANYE</a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="#about">TENTANG KAMI</a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="#donasi">DONASI</a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="#masuk">MASUK</a></li>
+					</ul>
+				</div>
+		</div>
+	</nav>
+
+	<!-- Masthead-->
+	<header class="masthead">
+		<!-- Carousel -->
+		<div id="demo" class="carousel slide carousel-fade" data-ride="carousel">
+			<ul class="carousel-indicators">
+				<li data-target="#demo" data-slide-to="0" class="active"></li>
+				<li data-target="#demo" data-slide-to="1"></li>
+				<li data-target="#demo" data-slide-to="2"></li>
+			</ul>
+			<div class="carousel-inner w3-animate-opacity">
+				<div class="carousel-item active">
+					<img style="width:100%; height: 100vh; -webkit-filter:brightness(50%)" src=" assets/images/home/home.jpg" alt="0">
+					<div class="carousel-caption" data-aos="zoom-in">
+						<div class="containers h-100">
+							<div class="row h-100 align-items-center justify-content-center text-center">
+								<div class="col-lg-10 align-self-end">
+									<h1 class="text-white font-weight-bold">APA ITU iTERRA</h1>
+								</div>
+								<div class="col-lg-8 align-self-baseline">
+									<p class="text-white-75 font-weight-light mb-5">Ini Tentang Aku, Kamu, dan Lingkungan Kita</p>
+									<img src="assets\images\logo\iterra_bulet-01.png" style="width: 100px;">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img style="width:100%; height: 100vh; -webkit-filter:brightness(50%)" src="assets/images/home/home2.jpg" alt="1" width="2100" height="500">
+					<div class="carousel-caption">
+						<div class="containers h-100">
+							<div class="row h-100 align-items-center justify-content-center text-center">
+								<div class="col-lg-10 align-self-end">
+									<h1 class="text-white font-weight-bold">KENAL LEBIH DEKAT DENGAN LINGKUNGAN</h1>
+								</div>
+								<div class="col-lg-8 align-self-baseline">
+									<p class="text-white-75 font-weight-light mb-5">Bumi layaknya seorang manusia yang membutuhkan perhatian.</p>
+									<img src="assets\images\logo\iterra_bulet-01.png" style="width: 100px;">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<img style="width:100%; height: 100vh; -webkit-filter:brightness(50%)" src="assets/images/home/home3.jpg" alt="2" width="2100" height="500">
+					<div class="carousel-caption">
+						<div class="containers h-100">
+							<div class="row h-100 align-items-center justify-content-center text-center">
+								<div class="col-lg-10 align-self-end">
+									<h1 class="text-white font-weight-bold">AYO BERGERAK BERSAMA KAMI</h1>
+								</div>
+								<div class="col-lg-8 align-self-baseline">
+									<p class="text-white-75 font-weight-light mb-5">Jika kita masih bisa berikan usaha kita untuk Bumi, mengapa harus mencari yang lain?</p>
+									<img src="assets\images\logo\iterra_bulet-01.png" style="width: 100px;">
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#demo" data-slide="prev">
+				<span class="carousel-control-prev-icon"></span>
+			</a>
+			<a class="carousel-control-next" href="#demo" data-slide="next">
+				<span class="carousel-control-next-icon"></span>
+			</a>
+		</div>
+	</header>
+
+	<!--Highlight-->
+	<section class="page-section bg-light" id="highlight">
+		<div class="container">
+			<div class="text-center section-title" data-aos="fade-down">
+				<h2 class="section-heading text-uppercase">highlight</h2>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-4 col-sm-6 mb-4" data-aos="zoom-in">
+					<div class="highlight-item">
+						<a class="highlight-link" data-toggle="modal" href="#highlightModal1">
+							<div class="img-hover-zoom">
+								<img src="assets/images/home/artikel1.jpg" alt="" />
+							</div>
+						</a>
+						<a href="#highlightModal1">
+							<div class="highlight-caption">
+								<div class="highlight-caption-heading">Category 1</div>
+								<div class="highlight-caption-subheading text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget pellentesque tortor. Nullam imperdiet sodales ipsum vel consectetur. Etiam non sodales justo, non volutpat lacus.</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 mb-4" data-aos="zoom-in">
+					<div class="highlight-item">
+						<a class="highlight-link" data-toggle="modal" href="#highlightModal1">
+							<div class="img-hover-zoom">
+								<img src="assets/images/home/artikel2.jpg" alt="" />
+							</div>
+						</a>
+						<a href="#highlightModal1">
+							<div class="highlight-caption">
+								<div class="highlight-caption-heading">Category 2</div>
+								<div class="highlight-caption-subheading text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget pellentesque tortor. Nullam imperdiet sodales ipsum vel consectetur. Etiam non sodales justo, non volutpat lacus.</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 mb-4" data-aos="zoom-in">
+					<div class="highlight-item">
+						<a class="highlight-link" data-toggle="modal" href="#highlightModal1">
+							<div class="img-hover-zoom">
+								<img src="assets/images/home/artikel3.jpg" alt="" />
+							</div>
+						</a>
+						<a href="#highlightModal1">
+							<div class="highlight-caption">
+								<div class="highlight-caption-heading">Category 3</div>
+								<div class="highlight-caption-subheading text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget pellentesque tortor. Nullam imperdiet sodales ipsum vel consectetur. Etiam non sodales justo, non volutpat lacus.</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 mb-4 mb-lg-0" data-aos="zoom-in">
+					<div class="highlight-item">
+						<a class="highlight-link" data-toggle="modal" href="#highlightModal1">
+							<div class="img-hover-zoom">
+								<img src="assets/images/home/artikel4.jpg" alt="" />
+							</div>
+						</a>
+						<a href="#highlightModal1">
+							<div class="highlight-caption">
+								<div class="highlight-caption-heading">Category 4</div>
+								<div class="highlight-caption-subheading text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget pellentesque tortor. Nullam imperdiet sodales ipsum vel consectetur. Etiam non sodales justo, non volutpat lacus.</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6 mb-4 mb-sm-0" data-aos="zoom-in">
+					<div class="highlight-item">
+						<a class="highlight-link" data-toggle="modal" href="#highlightModal1">
+							<div class="img-hover-zoom">
+								<img style="width: 200%;" src="assets/images/home/artikel5.jpg" alt="" />
+							</div>
+						</a>
+						<a href="#highlightModal1">
+							<div class="highlight-caption">
+								<div class="highlight-caption-heading">Category 5</div>
+								<div class="highlight-caption-subheading text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget pellentesque tortor. Nullam imperdiet sodales ipsum vel consectetur. Etiam non sodales justo, non volutpat lacus.</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-sm-6" data-aos="zoom-in">
+					<div class="highlight-item">
+						<a class="highlight-link" data-toggle="modal" href="#highlightModal1">
+							<div class="img-hover-zoom">
+								<img src="assets/images/home/artikel6.jpg" alt="" />
+							</div>
+						</a>
+						<a href="#highlightModal1">
+							<div class="highlight-caption">
+								<div class="highlight-caption-heading">Category 6</div>
+								<div class="highlight-caption-subheading text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget pellentesque tortor. Nullam imperdiet sodales ipsum vel consectetur. Etiam non sodales justo, non volutpat lacus.</div>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Artikel -->
+	<section id="artikel" class="artikel">
+		<div class="container">
+			<div class="text-center section-title" data-aos="fade-in">
+				<h2 style="color: #fff;">artikel</h2>
+			</div>
+			<div>
+				<div class="row content">
+					<!-- big -->
+					<div class="col-lg-6" data-aos="fade-in">
+						<div class="artikel-item artikel-hover-zoom">
+							<a href="#">
+								<img src="assets\images\home\artikel1.jpg">
+							</a>
+						</div>
+						<div data-aos="fade-in" class="artikel-item artikel-caption">
+							<a href="">
+								<h5>1 Januari 2021</h5>
+								<h3>
+									Vaksin Virus Korona Telah Ditemukan
+								</h3>
+							</a>
+
+						</div>
+					</div>
+
+					<div class="col-lg-6 pt-4 pt-lg-0 artikel-item">
+						<div class="artikel-item">
+							<div class="section-title" data-aos="fade-in">
+								<a href="">
+									<h3 style="font-size:x-large;">terbaru</h3>
+								</a>
+
+							</div>
+						</div>
+
+						<div class="row content">
+							<!-- mini 1 -->
+							<div class="col-lg-6 artikel-item artikel-hover-zoom-mini" data-aos="fade-in">
+								<a href="#">
+									<img src="assets\images\home\artikel2.jpg" style="width: 280px; padding:1rem">
+								</a>
+							</div>
+							<div class="col-lg-6 artikel-item" data-aos="fade-in">
+								<div class="artikel-caption">
+									<a href="">
+										<h5>1 Januari 2021</h5>
+										<h4>
+											Vaksin Virus Korona Telah Ditemukan
+										</h4>
+									</a>
+								</div>
+							</div>
+
+							<!-- mini 2 -->
+							<div class="col-lg-6 artikel-item artikel-hover-zoom-mini">
+								<a href="#">
+									<img src="assets\images\home\artikel2.jpg" style="width: 280px; padding:1rem">
+								</a>
+							</div>
+							<div class="col-lg-6 artikel-item" data-aos="fade-in">
+								<div class="artikel-caption">
+									<a href="">
+										<h5>1 Januari 2021</h5>
+										<h4>
+											Vaksin Virus Korona Telah Ditemukan
+										</h4>
+									</a>
+								</div>
+							</div>
+
+							<!-- mini 3 -->
+							<div class="col-lg-6 artikel-item artikel-hover-zoom-mini" data-aos="fade-in">
+								<a href="#">
+									<img src="assets\images\home\artikel2.jpg" style="width: 280px; padding:1rem">
+								</a>
+							</div>
+							<div class="col-lg-6 artikel-item" data-aos="fade-in">
+								<div class="artikel-caption">
+									<a href="">
+										<h5>1 Januari 2021</h5>
+										<h4>
+											Vaksin Virus Korona Telah Ditemukan
+										</h4>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- About Us-->
+	<section id="about" class="about">
+		<div class="container">
+			<div class="section-title" data-aos="fade-down">
+				<h2>Ada Apa Dengan Lingkungan?</h2>
+			</div>
+			<div class="row content">
+				<div class="col-lg-6" data-aos="fade-right">
+					<img src="assets\images\logo\iterra-01.png" style="width: 300px;">
+				</div>
+				<div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-left">
+					<p>
+						Di Indonesia masalah kebersihan lingkungan selalu menjadi perdebatan dan juga masalah yang terus berkembang. Mayoritas masyarakat Indonesia ternyata tak peduli akan kebersihan. Hal itu berdampak pada lingkungan sekitar dan juga kesehatan. Dari data riset Kementerian Kesehatan diketahui hanya
+						<b>20%</b>
+						dari total masyarakat Indonesia peduli terhadap kebersihan dan kesehatan. Ini berarti, dari 262 juta jiwa di Indonesia, hanya sekitar <b>52 juta orang</b> yang memiliki kepedulian terhadap kebersihan lingkungan sekitar dan dampaknya terhadap kesehatan.
+					</p>
+					<a class="button button-light button-xl js-scroll-trigger" href="#services">Baca Selengkapnya</a>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!--Footer-->
+	<section id="footer">
+		<footer>
+			<div class="footer-top">
+				<div class="containers">
+					<div class="row">
+						<div class="col-lg-4 col-md-6">
+							<div class="footer-info">
+								<img src="assets\images\logo\iterra-01.png" style="width: 100px;">
+								<h3>Tentang Kami</h3>
+								<p>
+									<strong>Alamat:</strong> Jalan Raya Bandung-Sumedang <br>
+									No. 239 Jatinangor <br>
+									<strong>Email:</strong> support@iterra.com<br>
+								</p>
+								<div class="social-links mt-3">
+									<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+									<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+									<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-6 footer-links">
+							<h4>Kategori</h4>
+							<ul>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Tentang Kami</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Artikel</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Kampanye</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Donasi</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-2 col-md-6 footer-links">
+							<h4>Update Terkini</h4>
+							<ul>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 1</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 2</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 3</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 4</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 5</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-4 col-md-6 footer-newsletter">
+							<h4>Langganan</h4>
+							<p>Dapatkan info terbaru dari Kami melalui e-mail</p>
+							<form action="" method="post">
+								<input type="email" name="email" placeholder="Email"><input type="submit" value="Subscribe">
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="copyright">
+					&copy; 2020 Copyright. All Rights Reserved
+				</div>
+				<div class="credits">
+					Developed and Designed by <a href="#about">iTerra</a>
+				</div>
+			</div>
+		</footer><!-- End Footer -->
+	</section>
+
+	<!-- Bootstrap core JS-->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Third party plugin JS-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+
+	<!-- Core theme JS-->
+	<script src="js/scripts.js"></script>
+
+	<!-- JS (includes Bootstrap)-->
+	<script src="js/main.js"></script>
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+	<!-- Animate on Screen -->
+	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+	<script>
+		AOS.init();
+	</script>
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<script>
+			$(document).ready(function() {
+				$("#preloader").fadeOut();
+			});
+		</script>
+	</div>
+
+
+</body>
+
+</html>
