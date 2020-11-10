a5df9d9082c51eb2fcb68dba7e91faf7dcc9f1a5
fauzanakmalh1 <fauzanakmalhariz1@gmail.com>
2020-11-10 23:08:06 +0700

    add donasi page + edit kampanye page aja

---
+++ application/controllers/Donasi.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Donasi extends CI_Controller {
+	
+	// Index
+	public function index() {
+		$this->load->view('donasi_view');
+	}
+	
+	// Fungsi lain
+	
+}
--- application/views/donasi_view.php
+++ application/views/donasi_view.php
@@ -52,27 +52,32 @@
 		</div>
 	</nav>
 
-	<!-- Kampanye-->
-	<section class="page-section bg-light kampanye" id="kampanye">
+	<!-- Donasi-->
+	<section class="page-section bg-light donasi" id="donasi">
 		<div class="container">
-			<div class="text-center section-title kampanye-title" data-aos="fade-down">
-				<h2 class="section-heading">Kampanye</h2>
+			<div class="text-center section-title donasi-title" data-aos="fade-down">
+				<h2 class="section-heading">Donasi</h2>
 			</div>
 
 			<!-- Contents -->
 			<div class="row">
 				<!-- Contents 1 -->
 				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
-					<div class="kampanye-item">
-						<a class="kampanye-link" data-toggle="modal" href="#kampanyeModal1">
+					<div class="donasi-item">
+						<a class="donasi-link" data-toggle="modal" href="#donasiModal1">
 							<div class="img-hover-zoom">
 								<img src="assets/images/home/artikel1.jpg" alt="" />
 							</div>
 						</a>
-						<a href="#kampanyeModal1">
-							<div class="kampanye-caption">
-								<div class="kampanye-caption-heading">Kampanye 1</div>
-								<a href="#kampanyeModal1" class="btn btn-detail">Detail ➔</a>
+						<a href="#donasiModal1">
+							<div class="donasi-caption">
+								<div class="donasi-caption-heading">Donasi 1</div>
+								<div class="progress mt-4 mb-2">
+									<div class="progress-bar bg-secondary" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<p class="donasi-value">Rp. 50.000.000</p> 
+								<p class="donasi-day">60 hari lagi</p>
+								<a href="#donasiModal1" class="btn btn-detail">Detail ➔</a>
 							</div>
 						</a>
 					</div>
@@ -80,16 +85,21 @@
 
 				<!-- Contents 2 -->
 				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
-					<div class="kampanye-item">
-						<a class="kampanye-link" data-toggle="modal" href="#kampanyeModal1">
+					<div class="donasi-item">
+						<a class="donasi-link" data-toggle="modal" href="#donasiModal1">
 							<div class="img-hover-zoom">
 								<img src="assets/images/home/artikel2.jpg" alt="" />
 							</div>
 						</a>
-						<a href="#kampanyeModal1">
-							<div class="kampanye-caption">
-								<div class="kampanye-caption-heading">Kampanye 2</div>
-								<a href="#kampanyeModal1" class="btn btn-detail">Detail ➔</a>
+						<a href="#donasiModal1">
+							<div class="donasi-caption">
+								<div class="donasi-caption-heading">Donasi 2</div>
+								<div class="progress mt-4 mb-2">
+									<div class="progress-bar bg-secondary" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<p class="donasi-value">Rp. 150.000.000</p> 
+								<p class="donasi-day">60 hari lagi</p>
+								<a href="#donasiModal1" class="btn btn-detail">Detail ➔</a>
 							</div>
 						</a>
 					</div>
@@ -97,16 +107,21 @@
 
 				<!-- Contents 3 -->
 				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
-					<div class="kampanye-item">
-						<a class="kampanye-link" data-toggle="modal" href="#kampanyeModal1">
+					<div class="donasi-item">
+						<a class="donasi-link" data-toggle="modal" href="#donasiModal1">
 							<div class="img-hover-zoom">
 								<img src="assets/images/home/artikel3.jpg" alt="" />
 							</div>
 						</a>
-						<a href="#kampanyeModal1">
-							<div class="kampanye-caption">
-								<div class="kampanye-caption-heading">Kampanye 3</div>
-								<a href="#kampanyeModal1" class="btn btn-detail">Detail ➔</a>
+						<a href="#donasiModal1">
+							<div class="donasi-caption">
+								<div class="donasi-caption-heading">Donasi 3</div>
+								<div class="progress mt-4 mb-2">
+									<div class="progress-bar bg-secondary" role="progressbar" style="width: 45%" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<p class="donasi-value">Rp. 10.000.000</p> 
+								<p class="donasi-day">60 hari lagi</p>
+								<a href="#donasiModal1" class="btn btn-detail">Detail ➔</a>
 
 							</div>
 						</a>
@@ -115,16 +130,21 @@
 
 				<!-- Contents 4 -->
 				<div class="col-lg-4 col-12 mb-4 mb-lg-0" data-aos="zoom-in">
-					<div class="kampanye-item">
-						<a class="kampanye-link" data-toggle="modal" href="#kampanyeModal1">
+					<div class="donasi-item">
+						<a class="donasi-link" data-toggle="modal" href="#donasiModal1">
 							<div class="img-hover-zoom">
 								<img src="assets/images/home/artikel4.jpg" alt="" />
 							</div>
 						</a>
-						<a href="#kampanyeModal1">
-							<div class="kampanye-caption">
-								<div class="kampanye-caption-heading">Kampanye 4</div>
-								<a href="#kampanyeModal1" class="btn btn-detail">Detail ➔</a>
+						<a href="#donasiModal1">
+							<div class="donasi-caption">
+								<div class="donasi-caption-heading">Donasi 4</div>
+								<div class="progress mt-4 mb-2">
+									<div class="progress-bar bg-secondary" role="progressbar" style="width: 55%" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<p class="donasi-value">Rp. 50.000.000</p> 
+								<p class="donasi-day">60 hari lagi</p>
+								<a href="#donasiModal1" class="btn btn-detail">Detail ➔</a>
 							</div>
 						</a>
 					</div>
@@ -132,16 +152,21 @@
 
 				<!-- Contents 5 -->
 				<div class="col-lg-4 col-12 mb-4 mb-sm-0" data-aos="zoom-in">
-					<div class="kampanye-item">
-						<a class="kampanye-link" data-toggle="modal" href="#kampanyeModal1">
+					<div class="donasi-item">
+						<a class="donasi-link" data-toggle="modal" href="#donasiModal1">
 							<div class="img-hover-zoom">
 								<img style="width: 200%;" src="assets/images/home/artikel5.jpg" alt="" />
 							</div>
 						</a>
-						<a href="#kampanyeModal1">
-							<div class="kampanye-caption">
-								<div class="kampanye-caption-heading">Kampanye 5</div>
-								<a href="#kampanyeModal1" class="btn btn-detail">Detail ➔</a>
+						<a href="#donasiModal1">
+							<div class="donasi-caption">
+								<div class="donasi-caption-heading">Donasi 5</div>
+								<div class="progress mt-4 mb-2">
+									<div class="progress-bar bg-secondary" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<p class="donasi-value">Rp. 150.000.000</p> 
+								<p class="donasi-day">60 hari lagi</p>
+								<a href="#donasiModal1" class="btn btn-detail">Detail ➔</a>
 							</div>
 						</a>
 					</div>
@@ -149,16 +174,21 @@
 
 				<!-- Contents 6 -->
 				<div class="col-lg-4 col-sm-6" data-aos="zoom-in">
-					<div class="kampanye-item">
-						<a class="kampanye-link" data-toggle="modal" href="#kampanyeModal1">
+					<div class="donasi-item">
+						<a class="donasi-link" data-toggle="modal" href="#donasiModal1">
 							<div class="img-hover-zoom">
 								<img src="assets/images/home/artikel6.jpg" alt="" />
 							</div>
 						</a>
-						<a href="#kampanyeModal1">
-							<div class="kampanye-caption">
-								<div class="kampanye-caption-heading">Kampanye 6</div>
-								<a href="#kampanyeModal1" class="btn btn-detail">Detail ➔</a>
+						<a href="#donasiModal1">
+							<div class="donasi-caption">
+								<div class="donasi-caption-heading">Donasi 6</div>
+								<div class="progress mt-4 mb-2">
+									<div class="progress-bar bg-secondary" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+								</div>
+								<p class="donasi-value">Rp. 10.000.000</p> 
+								<p class="donasi-day">60 hari lagi</p>
+								<a href="#donasiModal1" class="btn btn-detail">Detail ➔</a>
 							</div>
 						</a>
 					</div>
