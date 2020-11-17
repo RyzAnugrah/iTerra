6736bb4a3ce1617b57abe760327bf412ebdf212f
fauzanakmalh1 <fauzanakmalhariz1@gmail.com>
2020-11-17 13:34:07 +0700

    update about page

---
+++ application/views/about_view.php
@@ -0,0 +1,270 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<title>iTerra</title>
+	<link rel="icon" type="image/logo_title" href="assets\images\logo\iterra_bulet-01.png">
+
+	<!--Fonts-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" type="text/css">
+
+	<!-- CSS BUNDLE -->
+
+	<!--Third party plugin CSS-->
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+
+	<!-- Core theme JS-->
+	<link rel="stylesheet" href="css/styles.css" />
+
+	<!--CSS (includes Bootstrap)-->
+	<link rel="stylesheet" href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css'>
+	<link rel="stylesheet" href="https://unpkg.com/aos@2.3.1/dist/aos.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+	<style>
+		/* Back to the Top */
+		html {
+			scroll-behavior: smooth;
+		}
+	</style>
+</head>
+
+<body id="page-top">
+	<!-- Navigation-->
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+		<div class="containers">
+			<a class="navbar-brand js-scroll-trigger" href="home"><img src="assets\images\logo\iterra-01.png" style="width: 100px;" </a> <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto my-2 my-lg-0">
+						<li class="nav-item"><a class="nav-link js-scroll-trigger"></a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="artikel">ARTIKEL</a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="kampanye">KAMPANYE</a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="about">TENTANG KAMI</a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="donasi">DONASI</a></li>
+						<li class="nav-item"><a style="font-size:large;" class="nav-link js-scroll-trigger" href="login">MASUK</a></li>
+					</ul>
+				</div>
+		</div>
+	</nav>
+
+	<!-- Tentang Kami-->
+	<section class="page-section bg-light about" id="about">
+		<div class="container">
+			<div class="text-center section-title about-title" data-aos="fade-down">
+				<h2 class="section-heading">Tentang Kami</h2>
+			</div>
+
+			<div class="row">
+				<!-- Alvin -->
+				<div class="col-md-4 mb-4" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="img-hover-zoom">
+							<img class="img-hover-zoom" src="assets/images/logo/iterra_bulet-01.png" alt="Logo iTerra"/>
+						</div>
+						<div class="about-caption">
+							<div class="about-caption-heading">Logo iTerra</div>
+						</div>
+						<div class="img-hover-zoom">
+							<img class="img-hover-zoom" src="assets/images/logo/iterra-01.png" alt="Logo iTerra"/>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-8 mb-4" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="about-caption">
+							<div class="about-caption-heading">Apa itu iTerra?</div>
+							<div class="about-caption-subheading text-justify">iTerra berasal dari bahasa inggris yaitu Terra yang artinya bumi dan iteration yang artinya metode yang berulang. Jadi dengan adanya iTerra ini diharapkan kita sebagai manusia bisa terus-menerus menjaga, memelihara, dan melindungi lingkungan di sekitar kita. iTerra merupakan sebuah platform kampanye dan edukasi yang didesain semenarik mungkin untuk masyarakat agar lebih peduli terhadap lingkungan sekitarnya.</div> <br>
+							<div class="about-caption-heading">Latar belakang terciptanya iTerra?</div>
+							<div class="about-caption-subheading text-justify">Indonesia khususnya, masalah kebersihan lingkungan tersebut selalu menjadi perdebatan dan juga masalah yang terus berkembang. Mayoritas masyarakat Indonesia ternyata tak peduli akan kebersihan. Hal itu berdampak pada lingkungan sekitar dan juga kesehatan. Dari data riset Kementerian Kesehatan diketahui hanya 20 persen dari total masyarakat Indonesia peduli terhadap kebersihan dan kesehatan. Ini berarti, dari 262 juta jiwa di Indonesia, hanya sekitar 52 juta orang yang memiliki kepedulian terhadap kebersihan lingkungan sekitar dan dampaknya terhadap kesehatan. Hal tersebut bahkan dapat dilihat di dalam kebiasaan sehari-hari dan pola sanitasi masyarakat Indonesia. Misalnya, masih banyak masyarakat Indonesia yang tidak memiliki jamban dan fasilitas sanitasi yang memadai di tempat tinggalnya. Oleh karena itu, kami ingin membuat sebuah platform yang akan mengedukasi dan mengajak berkampanye mengenai lingkungan dengan sistem yang menarik dan inovatif karena kurangnya edukasi kepada masyarakat untuk peduli terhadap lingkungan. Sehingga platform ini akan mengedukasi dan mengajak berkampanye mengenai lingkungan dengan sistem yang menarik dan inovatif, setiap member yang aktif berkontribusi akan mendapatkan beragam keuntungan. Dan juga akan mengedukasi masyarakat terhadap peduli lingkungan.</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Contents -->
+			<div class="row">
+				<div class="col-md-12" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="about-caption">
+							<div class="about-caption-heading">iTerra - IT Developer</div>
+						</div>
+					</div>
+				</div>
+				<!-- Alvin -->
+				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="img-hover-zoom">
+							<img class="img-hover-zoom" src="assets/images/about/180013.png" alt="Alvin"/>
+						</div>
+						<div class="about-caption">
+							<div class="about-caption-heading">Alvin</div>
+						</div>
+					</div>
+				</div>
+				<!-- Anu -->
+				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="img-hover-zoom">
+							<img class="img-hover-zoom" src="assets/images/about/180049.png" alt="Anu"/>
+						</div>
+						<div class="about-caption">
+							<div class="about-caption-heading">Rizky Anugerah</div>
+						</div>
+					</div>
+				</div>
+				<!-- Fauzan -->
+				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="img-hover-zoom">
+							<img class="img-hover-zoom" src="assets/images/about/180005.png" alt="Fauzan"/>
+						</div>
+						<div class="about-caption">
+							<div class="about-caption-heading">Fauzan Akmal Hariz</div>
+						</div>
+					</div>
+				</div>
+				<!-- Naufal -->
+				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="img-hover-zoom">
+							<img class="img-hover-zoom" src="assets/images/about/180009.png" alt="Naufal"/>
+						</div>
+						<div class="about-caption">
+							<div class="about-caption-heading">Naufal Ariful Amri</div>
+						</div>
+					</div>
+				</div>
+				<!-- Archi -->
+				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="img-hover-zoom">
+							<img class="img-hover-zoom" src="assets/images/about/180050.png" alt="Archi"/>
+						</div>
+						<div class="about-caption">
+							<div class="about-caption-heading">Archi Cantona</div>
+						</div>
+					</div>
+				</div>
+				<!-- Delvian-->
+				<div class="col-lg-4 col-12 mb-4" data-aos="zoom-in">
+					<div class="about-item">
+						<div class="img-hover-zoom">
+							<img class="img-hover-zoom" src="assets/images/about/180065.png" alt="Delvian"/>
+						</div>
+						<div class="about-caption">
+							<div class="about-caption-heading">Delvian Ikhsan Maulana</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!--Footer-->
+	<section id="footer">
+		<footer>
+			<div class="footer-top">
+				<div class="containers">
+					<div class="row">
+						<div class="col-lg-4 col-md-6">
+							<div class="footer-info">
+								<img src="assets\images\logo\iterra-01.png" style="width: 100px;">
+								<h3>Tentang Kami</h3>
+								<p>
+									<strong>Alamat:</strong> Jalan Raya Bandung-Sumedang <br>
+									No. 239 Jatinangor <br>
+									<strong>Email:</strong> support@iterra.com<br>
+								</p>
+								<div class="social-links mt-3">
+									<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+									<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+									<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-2 col-md-6 footer-links">
+							<h4>Kategori</h4>
+							<ul>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Tentang Kami</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Artikel</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Kampanye</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Donasi</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-2 col-md-6 footer-links">
+							<h4>Update Terkini</h4>
+							<ul>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 1</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 2</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 3</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 4</a></li>
+								<li><i class="bx bx-chevron-right"></i> <a href="#">Update 5</a></li>
+							</ul>
+						</div>
+						<div class="col-lg-4 col-md-6 footer-newsletter">
+							<h4>Langganan</h4>
+							<p>Dapatkan info terbaru dari Kami melalui e-mail</p>
+							<form action="" method="post">
+								<input type="email" name="email" placeholder="Email"><input type="submit" value="Subscribe">
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="container">
+				<div class="copyright">
+					&copy; 2020 Copyright. All Rights Reserved
+				</div>
+				<div class="credits">
+					Developed and Designed by <a href="#about">iTerra</a>
+				</div>
+			</div>
+		</footer><!-- End Footer -->
+	</section>
+
+	<!-- JAVASCRIPT BUNDLE -->
+
+	<!-- Bootstrap core JS-->
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+
+	<!-- Third party plugin JS-->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+
+	<!-- Core theme JS-->
+	<script src="js/scripts.js"></script>
+	<script src="js/main.js"></script>
+	<script src="js/jquery-3.4.1.min.js"></script>
+
+	<!-- JS (includes Bootstrap)-->
+	<script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+	<script src="https://unpkg.com/boxicons@latest/dist/boxicons.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+	<!-- Animate on Screen -->
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+	<script>
+		AOS.init();
+	</script>
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<script>
+			$(document).ready(function() {
+				$("#preloader").fadeOut();
+			});
+		</script>
+	</div>
+
+</body>
+</html>
