6933364583ca2c87145b88ac34cd999852339601
Alvin <47733168+alvin2105@users.noreply.github.com>
2020-11-24 23:17:16 +0700

    syarat dan ketentuan

---
+++ application/views/syarat_ketentuan_view.php
@@ -0,0 +1,58 @@
+<section id="about" class="about" style="background-color: #f3f5fa;">
+		<div class="container">
+			
+			<div class="content">
+			<div class="section-title" data-aos="fade-down">
+				<br><br><h2>Syarat dan Ketentuan</h2>
+			</div>
+
+<p><span style="font-family: Helvetica;">Syarat dan ketentuan penggunaan ini <strong>(&quot;Ketentuan&quot;)</strong> merupakan perjanjian yang mengikat antara anda selaku pengguna situs www.iterra.com <strong>(&quot;Situs&quot;)</strong>ini beserta kami selaku pengelola situs, baik dalam posisi anda sebagai seorang member (anggota terdaftar) maupun visitor (anggota belum terdaftar)<br></span></p>
+<p><span style="font-family: Helvetica;">Ketentuan ini berlaku sehubungan dengan penggunaan Anda terhadap Situs ini, serta jasa yang kami berikan melalui situs ini&nbsp;</span></p>
+<p><span style="font-family: Helvetica;">Dengan Anda mempergunakan situs ini dengan cara apapun, atau Anda memanfaatkan Jasa dengan cara apapun, maka Anda dianggap :</span></p>
+<ol>
+    <li><span style="font-family: Helvetica;">Setuju untuk terikat dan tunduk pada semua syarat dan ketentuan sebagaimana di atur dalam ketentuan,serta</span></li>
+    <li><span style="font-family: Helvetica;">Bersedia bertanggungjawab terhadap setiap kelalaian maupun pelanggaran yang Anda lakukan sehubungan dengan kewajiban atau pernyataan Anda sebagaimana diatur dalam Ketentuan.</span></li>
+</ol>
+<p><span style="font-family: Helvetica;">&nbsp;</span></p>
+<p><span style="font-family: Helvetica;"><strong>Hak dan Kewajiban Anda Sebagai Pengguna</strong></span></p>
+<p><span style="font-family: Helvetica;">Dengan menggunakan Situs ini atau memanfaatkan Jasa, Anda menjamin dan menyatakan bahwa Anda cakap secara hukum dan terikat pada Ketentuan ini.</span></p>
+<ol>
+    <li><span style="font-family: Helvetica;">Anda berhak memanfaatkan seluruh fasilitas maupun Jasa yang tersedia di Situs, kecuali bagi fasilitas atau Jasa tertentu yang mengharuskan Anda memiliki akun.</span></li>
+    <li><span style="font-family: Helvetica;">Apabila Anda memilih untuk membuka akun, maka Anda dapat memperoleh fasilitas maupun Jasa tertentu yang mungkin tidak disediakan bagi pengguna Situs pada umumnya.</span></li>
+    <li><span style="font-family: Helvetica;">Anda tidak boleh memanfaatkan Situs maupun Jasa untuk keperluan-keperluan yang bertentangan dengan hukum, melanggar kesusilaan maupun melanggar hak Perseroan maupun hak pihak lainnya (baik hak pihak yang menggunakan Situs atau Jasa maupun hak dari pihak yang tidak menggunakan Situs maupun Jasa). Termasuk dalam hal ini Anda tidak dapat:</span></li>
+    <li><span style="font-family: Helvetica;">Melakukan atau menyuruh pihak lain untuk melakukan, tindakan apapun (termasuk mengunggah atau mendistribusikan piranti lunak, data atau program dalam bentuk apapun), yang dapat merusak, mengganggu atau membatasi kinerja Situs maupun Jasa, atau yang dapat menyebabkan fungsi-fungsi tertentu dalam Situs maupun Jasa menjadi tidak bekerja, atau yang dapat merubah fungsi, data atau program dari Situs maupun Jasa.</span></li>
+    <li><span style="font-family: Helvetica;">Memberikan, atau menyuruh pihak lain untuk memberikan, data atau informasi yang tidak benar atau menyesatkan, atau memalsukan data atau keterangan pihak lain</span></li>
+    <li><span style="font-family: Helvetica;">Melakukan atau menyuruh pihak lain untuk melakukan, tindakan apapun yang dapat menyebabkan pelanggaran terhadap hak milik intelektual iTerra, Member iTerra maupun visitor lain.,<br></span></li>
+</ol>
+<p><span style="font-family: Helvetica;">&nbsp;</span></p>
+<p><span style="font-family: Helvetica;"><strong>iTerra</strong><strong>&nbsp;tidak bertanggungjawab terhadap setiap tindakan Anda dalam memanfaatkan situs , dan Anda menyatakan dan menjamin akan membebaskan iTerra dari segala tuntutan yang muncul sebagai akibat penggunaan Anda terhadap Situs ini.</strong></span></p>
+<p><span style="font-family: Helvetica;">&nbsp;</span></p>
+<p><span style="font-family: Helvetica;"><strong>iTerra tidak menjamin pada beberapa hal berikut :</strong></span></p>
+<ol>
+    <li><span style="font-family: Helvetica;">Pelaksanaan Kampanye &nbsp;maupun terpenuhinya Kampanye sebagaimana dijanjikan oleh Pemilik Kampanye ;</span></li>
+    <li><span style="font-family: Helvetica;">Tersedianya maupun terpenuhinya barang, jasa, layanan, fasilitas atau imbal balik apapun dari Pemilik Proyek kepada Anda atas bantuan yang Anda berikan;</span></li>
+    <li><span style="font-family: Helvetica;">Terpenuhinya semua reward atau janji-janji yang diberikan Pemilik Kampanye Alam, baik yang dinyatakan secara langsung maupun yang diberikan melalui Perseroan selaku penerima kuasa Pemilik Proyek.</span></li>
+</ol>
+<p><span style="font-family: Helvetica;">&nbsp;</span></p>
+<p><span style="font-family: Helvetica;"><strong>Ketentuan Pemanfaatan Fitur Platform iTerra</strong></span></p>
+<p><span style="font-family: Helvetica;">Donasi</span></p>
+<ol>
+    <li><span style="font-family: Helvetica;">Kegiatan donasi hanya dapat diikuti oleh pengguna yang sudah melakukan pendaftaran ke website<br></span></li>
+    <li><span style="font-family: Helvetica;">kegiatan donasi hanya dapat dibuat oleh admin iTerra</span></li>
+    <li><span style="font-family: Helvetica;">Semua bukti transfer dalam kegiatan donasi ini akan disimpan dalam database iTerra<br></span></li>
+    <li><span style="font-family: Helvetica;">perpanjangan waktu donasi tidak dapat dilakukan tanpa persetujuan admin iTerra</span></li>
+    <li><span style="font-family: Helvetica;">pastikan hanya mengikuti kegiatan donasi di website iTerra, karena iTerra <strong>tidak bertangggungjawab terhadap kegiatan transkasi di luar website</strong></span></li>
+</ol>
+<p><span style="font-family: Helvetica;">Kampanye</span></p>
+<ol>
+    <li><span style="font-family: Helvetica;">Kegiatan kegiatan hanya dapat diikuti oleh pengguna yang sudah melakukan pendaftaran ke website</span></li>
+    <li><span style="font-family: Helvetica;">kegiatan kampanye &nbsp;dapat dibuat oleh pengguna yang sudah melakukan pendaftaran dan melakukan verifikasi data<br></span></li>
+    <li><span style="font-family: Helvetica;">kegiatan kampanye harus disetujui admin iTerra agar dapat di publikasikan<br></span></li>
+    <li><span style="font-family: Helvetica;">semua kegiatan yang berasal dari website iTerra wajib untuk mencantumkan logo iTerra dalam kegiatan tersebut<br></span></li>
+    <li><span style="font-family: Helvetica;">iTerra tidak bertanggungjawab atas kegagalan kampanye yang dibuat oleh member terkecuali telah melakukan kontak khusu dengan iTerra</span></li>
+</ol>
+<p><span style="font-family: Helvetica;"><br></span></p>
+
+</div>
+		</div>
+	</section>
