ff8873d754aca010a1e64541c0e84812e4b0e3f9
Alvin <47733168+alvin2105@users.noreply.github.com>
2020-11-24 23:16:06 +0700

    syarat dan ketentuan

---
+++ application/controllers/Syarat_Ketentuan.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Syarat_Ketentuan extends CI_Controller {
+
+	// Index
+	public function index() {
+		$this->load->view('templates/header_view');
+		$this->load->view('syarat_ketentuan_view');
+		$this->load->view('templates/footer_view');
+	}
+
+	// Fungsi lain
+
+}
