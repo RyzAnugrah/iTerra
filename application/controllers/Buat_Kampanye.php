d3c334fe9b5f3734040cb2605e1f7806613f27da
RyzAnugrah <asep.anugerah50@gmail.com>
2020-11-24 21:41:31 +0700

    edit buat_kampanye

--- application/controllers/Buat_Kampanye.php
+++ application/controllers/Buat_Kampanye.php
@@ -11,16 +11,16 @@ class Buat_Kampanye extends CI_Controller {
 	// Fungsi lain
 	public function tambah() {
 		$judul = $this->input->post('judul_konten');
-		$tgl = $this->input->post('tanggal_donasi');
+		$tgl = $this->input->post('tanggal_konten');
 		$desk = $this->input->post('deskripsi');
 		$data = array(
 			'email' => ($_SESSION['email']),
-			'judul_donasi' => $judul,
-			'tanggal_donasi' => $tgl,
+			'judul_konten' => $judul,
+			'tanggal_konten' => $tgl,
 			'deskripsi' => $desk,
 			);
-		$this->db->insert('donasi', $data);
-		redirect(base_url('Dashboard_admin'));
+		$this->db->insert('kampanye', $data);
+		redirect(base_url('Dashboard_member'));
 	}
 	
 }
