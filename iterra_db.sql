1ac9f7c07108b04b1522646e607639efdb260096
RyzAnugrah <asep.anugerah50@gmail.com>
2020-12-09 03:27:50 +0700

    Update iterra_db.sql

--- iterra_db.sql
+++ iterra_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Des 2020 pada 21.16
+-- Waktu pembuatan: 08 Des 2020 pada 21.27
 -- Versi server: 10.1.38-MariaDB
 -- Versi PHP: 5.6.40
 
@@ -43,7 +43,7 @@ CREATE TABLE `artikel` (
 --
 
 INSERT INTO `artikel` (`kode_artikel`, `judul_artikel`, `deskripsi`, `tanggal`, `email`, `penulis`, `gambar`) VALUES
-(22, 'Artikel 1', '<p>Test 1</p>\r\n', '2020-12-01 09:35:02', 'admin@gmail.com', 'Jongkook', 'aeb76f0fe38266e150cc32fd1ea863b9.jpg');
+(24, 'Manfaat Pohon untuk Manusia dan Lingkungan ', '<p><a href=\"https://www.kompasiana.com/tag/gerakan-bumi-hijau\">Gerakan Bumi Hijau</a> ,- ada banyak manfaat Pohon untuk kehidupan Manusia dan Lingkungan .</p>\r\n\r\n<p>Beberapa Maanfaat itu adalah Membuat udara jadi lebih segar, Menjaga kesehatan mental , Mengurangi paparan sinar UV ke kulit , Mengurangi dampak Perubahan iklim , Mencegah Polusi Air , Menambah cadangan air tanah , Menjaga populasi makhluk hidup ,Tembat Habitat Satwa , Mencegah Banjir dan Erosi Tanah .</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<div id=\"teads_outstream\">&nbsp;</div>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>KEKERINGAN/BANJIR dan LONGSOR</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<div class=\"flying_desktop text-center\">&nbsp;</div>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>Kerusakan <a href=\"https://www.kompasiana.com/tag/hutan\">Hutan</a> dan Lingkungan adalah sumber masalah awal dari bencana alam seperti Banjir Bandang , Tanah Longsor , Kekeringan , dan Lain nya .</p>\r\n\r\n<p>Terkadang banyak yg peduli dengan musibah nya ketika sudah terjadi , tapi kurang nya kepedulian dalam hal antisipasi pencegahan nya .</p>\r\n\r\n<p>Perusakan semakin hari semakin sering terjadi , yg semesti nya harus kita jaga bersama untuk keseimbangan . Banyak yg menjadikan Alam sebagai Alat untuk meraup keuntungan pribadi tetapi tak memikirkan dampak besar bagi kehidupan makhluk hidup . Yg juga suatu hari dampak nya pasti akan dirasakan pada generasi mereka .</p>\r\n\r\n<p>Jadikan pohon untuk keseimbangan makhluk hidup dan lingkungan . Kita jaga bersama kelestarian nya , kita Tanam kembali jikalau di lingkungan kita ada kawasan&quot; krisis dari pohon yg rawan banjir , longsor ataupun kekeringan . Kita jaga bersama hutan atau alam yg ada di lingkungan kita , krna itu paruparu bumi kita . klu mereka rusak dan tercemar itu akan menjadi dampak buruk yg sangat besar bagi kita , bagi lingkungan kita dan bagi habitat para satwa .</p>\r\n\r\n<p>Satu pohon sangat berarti bagi Bumi, Save Forest , Save The Earth&nbsp;</p>\r\n', '2020-12-08 20:16:41', 'admin@gmail.com', 'JurnalJisoo', '11b835f5648104edb1ea1052dd670fa7.jpg');
 
 -- --------------------------------------------------------
 
@@ -63,28 +63,24 @@ CREATE TABLE `data_koin` (
 --
 
 CREATE TABLE `donasi` (
-  `kode_donasi` varchar(20) NOT NULL,
+  `kode_donasi` int(20) NOT NULL,
   `judul_donasi` varchar(50) NOT NULL,
   `deskripsi` varchar(500) NOT NULL,
   `tanggal_donasi` date NOT NULL,
   `kode_koin` varchar(20) NOT NULL,
   `jumlah_donasi` bigint(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `gambar` varchar(40) NOT NULL
+  `gambar` varchar(40) NOT NULL,
+  `nilai` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `donasi`
 --
 
-INSERT INTO `donasi` (`kode_donasi`, `judul_donasi`, `deskripsi`, `tanggal_donasi`, `kode_koin`, `jumlah_donasi`, `email`, `gambar`) VALUES
-('', 'dana sosial', '<p>aku sang pejuang</p>', '2020-11-21', '', 0, 'admin@gmail.com', ''),
-('1', 'Pelestarian Hutan sebagai Pencegahan Tanah Longsor', 'Pelestarian Hutan sebagai Pencegahan Tanah Longsor di Indonesia', '2020-11-22', '1', 100000000, 'naufalariful16@gmail.com', ''),
-('2', 'Pelestarian Hutan sebagai Pencegahan Banjir', 'Pelestarian Hutan sebagai Pencegahan Banjir di Indonesia', '2020-11-21', '2', 200000000, 'naufalariful16@gmail.com', ''),
-('3', 'hai', '<p>conoth lagiii</p>\r\n', '2020-12-02', '', 120, 'admin@gmail.com', 'ac1e5edcc269fa8929930943058798af.png'),
-('4', 'contoh lagiii', '<p>ini contoh saja</p>\r\n', '2020-12-03', '', 1000000000, 'admin@gmail.com', '91e5213662926f7a95ade0d3f8526b62.png'),
-('5', 'conoth lagiiiii dong', '<p>banyak amat contohnya</p>\r\n', '2020-12-05', '', 1000000, 'admin@gmail.com', '57edd14b73cd55ff000a27a259734693.png'),
-('6', '1', '<p>1</p>\r\n', '2020-12-12', '', 1, 'admin@gmail.com', 'e1ade9a57685301aa9e192c09a9d9f7d.jpg');
+INSERT INTO `donasi` (`kode_donasi`, `judul_donasi`, `deskripsi`, `tanggal_donasi`, `kode_koin`, `jumlah_donasi`, `email`, `gambar`, `nilai`) VALUES
+(1, 'Lebak Diterjang Banjir Dan Longsor, Bantu Segera!', '<p>Banjir yang melanda Lebak, Banten telah menelan korban jiwa. Sejak Sabtu 5/12/2020 kemarin hujan dengan intensitas tinggi meluapkan sungai-sungai yang mengalir di 14 kecamatan Lebak. Sekitar 1.200 rumah terendam dan puluhan rumah rusak berat.&nbsp;</p>\r\n\r\n<p><img class=\"img-fluid img-responsive\" src=\"https://img.kitabisa.cc/size/1000/ba460b3d-bf62-44f2-81e7-cad9127457ae.jpg\" style=\"border:0px none; box-sizing:inherit; display:block; height:auto !important; max-width:100%; outline:currentcolor', '2020-12-18', '', 200000000, 'admin@gmail.com', '5f165bc26f938dc7629514e1262184d3.jpg', 1),
+(2, 'Banjir Bandang Medan Ketinggian Air 2 Meter ', '<h3>Sumatera Utara kembali mengalami bencana alam banjir! Ketinggian air mencapai 2 meter membuat 2773 rumah terendam dan 5965 jiwa terdampak sehingga harus mengungsi ke tempat yang lebih aman.&nbsp;</h3>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p><img class=\"img-fluid img-responsive\" src=\"https://img.kitabisa.cc/size/1000/070dd578-f6cc-4f64-baae-9c8848388164.jpg\" title=\"\" /></p>\r\n\r\n<p>Hujan deras pada malam hari (3/12) hingga dini hari (4/12) membuat banjir setinggi 2 meter menimpa beberapa titik di Sumatera Utara', '2020-12-25', '', 30000000, 'admin@gmail.com', 'b80726a03b25e28757098bec5f64a1af.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -97,7 +93,7 @@ CREATE TABLE `identitas` (
   `nama` varchar(200) NOT NULL,
   `jenis_kelamin` varchar(20) NOT NULL,
   `nomortelepon` varchar(15) NOT NULL,
-  `fotoprofil` blob NOT NULL,
+  `fotoktp` varchar(255) NOT NULL,
   `NIK` varchar(30) NOT NULL,
   `password` varchar(70) NOT NULL,
   `tanggallahir` date NOT NULL,
@@ -109,11 +105,9 @@ CREATE TABLE `identitas` (
 -- Dumping data untuk tabel `identitas`
 --
 
-INSERT INTO `identitas` (`email`, `nama`, `jenis_kelamin`, `nomortelepon`, `fotoprofil`, `NIK`, `password`, `tanggallahir`, `admin`, `validasi`) VALUES
+INSERT INTO `identitas` (`email`, `nama`, `jenis_kelamin`, `nomortelepon`, `fotoktp`, `NIK`, `password`, `tanggallahir`, `admin`, `validasi`) VALUES
 ('admin@gmail.com', 'admin', 'pria', '08238585855', '', '10235629847', '$2y$10$gWFPHJD4bwkUCwzvrhgdCOEjNvMrqh4kJqsL10SfO02H/bJL1Bt36', '2020-11-01', 1, 1),
-('alvin18001@mail.unpad.ac.id', 'Alvin', 'pria', '089698916727', '', '123', '$2y$10$D93NOX4zz2hdK8kNSLA4xuOmvnXuvRjMJK5qzBaanEqi5O9UB5uJy', '2020-12-10', 0, 1),
-('naufal.ariful.amri@gmail.com', 'naufal ariful', 'pria', '08238585855', '', '10235629847', '$2y$10$uRuga4qthZOOZ/HZRMmEn.mWYjsrpxETcrdJVfsKR6oOi1w9.hMc2', '2020-11-02', 0, 0),
-('naufalariful16@gmail.com', 'naufal ariful', 'pria', '08238585855', '', '10235629847', '$2y$10$uRuga4qthZOOZ/HZRMmEn.mWYjsrpxETcrdJVfsKR6oOi1w9.hMc2', '2020-11-02', 0, 0);
+('member@gmail.com', 'Member', 'pria', '084679846587', 'c63f845e5df59dc116c4ecce1c045e50.jpg', '4568791340598756', '$2y$10$j0zI.p1CjnP8P7rVDMgMtezV7s426Gou1X1ToTsi9t.E3f3v8O/mC', '2020-12-01', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -138,18 +132,7 @@ CREATE TABLE `kampanye` (
 --
 
 INSERT INTO `kampanye` (`kode_kampanye`, `judul_kampanye`, `deskripsi`, `tanggal`, `kode_koin`, `email`, `gambar`, `verifikasi`, `penulis`) VALUES
-(3, '1', '<p>1</p>\r\n', '2020-12-11', '', 'naufal.ariful.amri@gmail.com', 'e6515aa4d4e7e0211f6c736fc53c6b9a.jpg', 1, '1'),
-(4, '2', '<p>2</p>\r\n', '2020-12-17', '', 'naufal.ariful.amri@gmail.com', 'd18ec6ad0c473108ce82dd012c457965.jpg', 1, '2'),
-(5, '3', '<p>3</p>\r\n', '2020-12-18', '', 'naufal.ariful.amri@gmail.com', '3037bbf3640c4df7bdd9eb9ba9bfdde7.jpg', 1, '3'),
-(6, '4', '<p>4</p>\r\n', '2020-12-19', '', 'naufal.ariful.amri@gmail.com', 'c5498e83608adb28b18067ffa3a92872.jpg', 1, '4'),
-(7, '5', '<p>5</p>\r\n', '2020-12-02', '', 'naufal.ariful.amri@gmail.com', 'ffb94d1ca3b441bfae76dfa66246898e.jpg', 1, '5'),
-(8, '6', '<p>6</p>\r\n', '2020-12-03', '', 'naufal.ariful.amri@gmail.com', 'b6779fcfb63f3a66157015f59d857b58.jpg', 1, '6'),
-(9, '7', '<p>7</p>\r\n', '2020-12-10', '', 'naufal.ariful.amri@gmail.com', '31262f84ad6c0a25203d81e7ad6d2916.jpg', 1, '7'),
-(10, '8', '<p>8</p>\r\n', '2020-12-16', '', 'naufal.ariful.amri@gmail.com', 'c1bee5689aee280f377e8d54a1dff767.jpg', 1, '8'),
-(11, '9', '<p>9</p>\r\n', '2020-12-03', '', 'naufal.ariful.amri@gmail.com', '6d8f436841a271536d5c216178fee303.jpg', 1, '9'),
-(12, '10', '<p>10</p>\r\n', '2020-12-02', '', 'naufal.ariful.amri@gmail.com', '894e86e79fc8ad5b16ab39c9fa45a387.jpg', 1, '10'),
-(13, '11', '<p>11</p>\r\n', '2020-12-09', '', 'naufal.ariful.amri@gmail.com', '105e38e297f18ba225d569744cb05037.jpg', 1, '11'),
-(14, 'tes', '<p>aa</p>\r\n', '2020-12-18', '', 'admin@gmail.com', '4a59e056abdd1120bc7da7b16aed5a3d.jpg', 1, 'ea');
+(15, 'Bantu Selamatkan Habitat Satwa dengan Menanam Pohon', '<p><em>Gerakan Bumi Hijau</em> , - <strong><a href=\"https://www.kompasiana.com/tag/satwa\">Satwa</a>&quot; <a href=\"https://www.kompasiana.com/tag/hutan\">Hutan</a> seperti Monyet dan Sejenis nya semakin hari semakin sering kita lihat keluar hutan untuk mencari makanan , dari mulai ke jalanan hingga sampai masuk ke laham dan permukiman masyarakat .</strong><br />\r\n<br />\r\nBegitu juga dengan jenis satwa&quot; liar lain nya seperti Burung&quot; hingga satwa buas. Salah satu faktor penyebab mungkin k', '2020-12-18', '', 'member@gmail.com', 'df924363b7438b141a572ccb91879cb7.jpg', 1, 'Lowangoalski');
 
 --
 -- Indexes for dumped tables
@@ -196,13 +179,19 @@ ALTER TABLE `kampanye`
 -- AUTO_INCREMENT untuk tabel `artikel`
 --
 ALTER TABLE `artikel`
-  MODIFY `kode_artikel` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `kode_artikel` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT untuk tabel `donasi`
+--
+ALTER TABLE `donasi`
+  MODIFY `kode_donasi` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `kampanye`
 --
 ALTER TABLE `kampanye`
-  MODIFY `kode_kampanye` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `kode_kampanye` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
